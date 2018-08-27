924af2ca24e1662dea1ad7492ff6e791c073ec5d
Muhozi Emery <muhozie@gmail.com>
2018-08-28 01:00:40 +0300

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <Helmet title="JAMStack hackathon" />
+    <h1>Hi,</h1>
+    <p>
+      I'm Emery Muhozi, a software developer, Tech enthusiast who believes in
+      human potential and its great impact on the world. I'm into inspired
+      self-taught learning. I like Open-source culture
+    </p>
+  </div>
+)
+
+export default IndexPage
